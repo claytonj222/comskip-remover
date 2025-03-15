86ee110f075ac43202efe27d4af9e9a229f87cf1
claytonj222 <42755804+claytonj222@users.noreply.github.com>
2025-03-15 16:36:08 -0400

    Update post-process.sh

--- post-process.sh
+++ post-process.sh
@@ -66,10 +66,6 @@ printf "${GREEN}path:${NC} ${__path}\ndir: ${__dir}\nbase: ${__base}\n"
 # Change to the directory containing the recording
 cd "${__dir}"
 
-# Extract closed captions to external SRT file
-printf "[post-process.sh] %bExtracting subtitles...%b\n" "$GREEN" "$NC"
-$__ccextractor "${__file}" -o "${__base}.en.srt"
-
 # Run comcut/comskip inside the container
 echo "Running Comcut with the following command:"
 echo "$__command --ffmpeg=$__ffmpeg --work-dir=\"${__dir}\" --comskip=/usr/bin/comskip --lockfile=/tmp/comchap.lock --comskip-ini=\"${__comskipini}\" \"${__file}\""
@@ -82,6 +78,11 @@ $__ffmpeg -i "${__file}" "${__base}.${__container}"
 # Remove the original recording file
 printf "[post-process.sh] %bRemoving original file...%b\n" "$GREEN" "$NC"
 rm "${__file}"
+
+# Extract closed captions to external SRT file
+printf "[post-process.sh] %bExtracting subtitles...%b\n" "$GREEN" "$NC"
+__file="${__base}.mkv"
+$__ccextractor "${__file}" -o "${__base}.en.srt"
 
 # Return to the starting directory
 cd "${PWD}"
